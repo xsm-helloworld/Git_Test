a2f7eae9a07c940618ce4cadfe3a0a04417d6e99
xsm <18268867701@163.com>
2022-11-23 22:33:32 +0800

    commit first

---
+++ src/main/java/com/xsm/git/App.java
@@ -0,0 +1,13 @@
+package com.xsm.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git!" );
+    }
+}
